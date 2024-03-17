43d82fb93470c26d42e5aaca0e97a08b00403478
yillachen <chn.yilla@gmail.com>
2024-03-16 22:56:48 -0400

    add: swap model and associations

--- server/db/index.js
+++ server/db/index.js
@@ -3,6 +3,8 @@ const Customer = require('./customer/customer');
 const Product = require('./product/product');
 const Swap = require('./swap/swap');
 const ChatGroup = require('./chat/chatGroup');
+const Chat = require('./chat/chat');
+const Message = require('./chat/message');
 const Address = require('./customer/address');
 
 // Sequelize Documentation on associations: https://sequelize.org/docs/v6/core-concepts/assocs/
@@ -16,13 +18,24 @@ ChatGroup.belongsToMany(Customer, {
   as: 'chat_groups'
 })
 
+ChatGroup.hasOne(Chat);
+Chat.belongsTo(ChatGroup);
+
+Chat.hasMany(Message);
+Message.belongsToMany(Chat, {
+  through: 'Chat_Message',
+  as: 'chat_messages'
+})
+
 Customer.hasMany(Address);
 Address.belongsTo(Customer);
 
-// Customer.hasMany(Swap);
-// Swap.hasMany(Customer);
+Customer.hasMany(Swap);
+Swap.belongsToMany(Customer, {
+  through: 'Customer_Swap',
+  as: 'swaps'
+})
 
 module.exports = {
-  db, Customer, Product, ChatGroup, Address
-  // Swap
+  db, Customer, Product, ChatGroup, Address, Swap
 }
